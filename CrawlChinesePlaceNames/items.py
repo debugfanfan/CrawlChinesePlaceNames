1852ef78278d3f436608ab225780ba0405c1d728
不凡不弃 <291622538@qq.com>
2020-05-20 16:09:23 +0800

    修改

--- CrawlChinesePlaceNames/items.py
+++ CrawlChinesePlaceNames/items.py
@@ -7,23 +7,18 @@ import scrapy
 
 
 class ChineseItem(scrapy.Item):
-    # 省
-    province_num = scrapy.Field()
-    province_name = scrapy.Field()
-    province_url = scrapy.Field()
-    # 市
-    city_num = scrapy.Field()
-    city_name = scrapy.Field()
-    city_url = scrapy.Field()
-    # 县
-    county_num = scrapy.Field()
-    county_name = scrapy.Field()
-    county_url = scrapy.Field()
-    # 镇
-    town_num = scrapy.Field()
-    town_name = scrapy.Field()
-    town_url = scrapy.Field()
-    # 村
-    village_num = scrapy.Field()
-    village_class = scrapy.Field()
-    village_name = scrapy.Field()
+    province_num = scrapy.Field()  # 省代号
+    province_name = scrapy.Field()  # 省名
+    province_url = scrapy.Field()  # 省url
+    city_num = scrapy.Field()  # 市代号
+    city_name = scrapy.Field()  # 市名
+    city_url = scrapy.Field()  # 市url
+    county_num = scrapy.Field()  # 县代号
+    county_name = scrapy.Field()  # 县名
+    county_url = scrapy.Field()  # 县url
+    town_num = scrapy.Field()  # 镇代号
+    town_name = scrapy.Field()  # 镇名
+    town_url = scrapy.Field()  # 镇url
+    village_num = scrapy.Field()  # 村代号
+    village_class = scrapy.Field()  # 村分类
+    village_name = scrapy.Field()  # 村名
